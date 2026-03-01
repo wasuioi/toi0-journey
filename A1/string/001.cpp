c4afd68d7c834b6e9ec44e7f86b52368a40b29f5
wasu <if.true.then.2537@gmail.com>
2026-03-01 23:36:39 +0700

    first step

---
+++ A1/string/001.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string a,b;
+    cin >> a >> b;
+    cout << "Hello " << a << " " << b << '\n';
+    cout << a[0] << a[1] << b[0] << b[1];
+    return 0;
+}
